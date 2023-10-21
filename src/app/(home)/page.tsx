701d29640ab6bc0572bbb9f463d0b135bd8ba19a
Thiago Sullivan <thiagosullivan12@gmail.com>
2023-10-21 06:01:17 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -26,8 +26,18 @@ export default async function Home() {
         <Categories />
       </div>
       <div className='mt-8'>
+        <p className='uppercase font-bold mb-3 pl-5'>Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        className='h-auto w-full px-5'
+        sizes="100vw"
+        alt="Até 55% desconto em mouses"
+      />
     </div>
   )
 }
