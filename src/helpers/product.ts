6156a6ed6ec312856ab67ccb3c3cf6c1f758df1f
Thiago Sullivan <thiagosullivan12@gmail.com>
2023-10-21 05:57:39 -0300

    fix: ensure correct product total price is calculated

--- src/helpers/product.ts
+++ src/helpers/product.ts
@@ -12,11 +12,11 @@ export const computeProductTotalPrice = (product: Product): ProductWithTotalPric
         }
     }
 
-    const totalPrice =
+    const totalDiscount =
         Number(product.basePrice) * (product.discountPercentage / 100);
 
     return {
         ...product,
-        totalPrice
+        totalPrice: Number(product.basePrice) - totalDiscount,
     }
 }
