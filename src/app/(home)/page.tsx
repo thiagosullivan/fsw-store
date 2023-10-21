562a678a456602d81cf1277642c7ad5981ac8a86
Thiago Sullivan <thiagosullivan12@gmail.com>
2023-10-21 06:15:14 -0300

    feat: add third PromoBanner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -23,29 +23,36 @@ export default async function Home() {
   })
 
   return (
-    <div>
+    <div className='flex flex-col gap-8'>
       <PromoBanner
         src="/banner-home-01.png"
         alt="Até 55% Desconto só esse mÊs"
       />
 
-      <div className='mt-8 px-5'>
+      <div className='px-5'>
         <Categories />
       </div>
 
-      <div className='mt-8'>
+      <div>
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
 
       <PromoBanner
         src="/banner-home-02.png"
-        alt="Até 55% desconto em mouses"
+        alt="Até 55% desconto em mouses!"
       />
 
-      <div className='mt-8'>
+      <div>
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
+      </div>
+
+      <div>
+        <PromoBanner
+          src="/banner-home-03.png"
+          alt="Até 55% desconto em fones!"
+        />
       </div>
     </div>
   )
